9ea5135592c484abc39e6c852f7a0b89f87a292b
GamingItamae <160610502+GamingItamae@users.noreply.github.com>
2024-12-11 22:18:58 +0900

    宴追加

---
+++ Assets/Utage/Scripts/TemplateUI/Gallery/UtageUguiSoundRoomItem.cs
@@ -0,0 +1,50 @@
+﻿// UTAGE: Unity Text Adventure Game Engine (c) Ryohei Tokimura
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Utage;
+using System;
+using TMPro;
+
+namespace Utage
+{
+
+	/// <summary>
+	/// サウンドルーム用のUIのサンプル
+	/// </summary>
+	[AddComponentMenu("Utage/TemplateUI/UtageUguiSoundRoomItem")]
+	public class UtageUguiSoundRoomItem : MonoBehaviour
+	{
+		/// <summary>本文</summary>
+		[HideIfTMP] public Text title;
+
+		[HideIfLegacyText] public TextMeshProUGUI titleTmp;
+
+
+		public AdvSoundSettingData Data
+		{
+			get { return data; }
+		}
+
+		protected AdvSoundSettingData data;
+
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		/// <param name="data">セーブデータ</param>
+		/// <param name="index">インデックス</param>
+		public virtual void Init(AdvSoundSettingData data, Action<UtageUguiSoundRoomItem> ButtonClickedEvent, int index)
+		{
+			this.data = data;
+			SetTextTitle(data.LocalizedTitle);
+
+			UnityEngine.UI.Button button = this.GetComponent<UnityEngine.UI.Button>();
+			button.onClick.AddListener(() => ButtonClickedEvent(this));
+		}
+
+		public virtual void SetTextTitle(string text)
+		{
+			TextComponentWrapper.SetText(title, titleTmp, text);
+		}
+	}
+}
