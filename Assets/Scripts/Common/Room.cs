9cfb485c1736548f2471d6c7de3345d91b7a1baf
sukuofu <sukuofu.xof@gmail.com>
2025-05-22 22:00:02 +0900

    ゲームシステム 部屋移動の機能途中

--- Assets/Scripts/Common/Room.cs
+++ Assets/Scripts/Common/Room.cs
@@ -4,62 +4,147 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    [SerializeField]
-    public List<Room> MovableRooms;
-
-    private GameCanvas _gameCanvas;
-
-    private GameCanvas gameCanvas
+    /// <summary>
+    /// 部屋の記号
+    /// 参考: https://traio.hatenablog.com/entry/2020/06/11/143656
+    /// </summary>
+    public enum RoomSymbol
     {
-        get
-        {
-            if (!_gameCanvas) _gameCanvas = GameObject.FindAnyObjectByType<GameCanvas>();
-            return _gameCanvas;
-        }
+        A, B, C, D, E, F, G, H, I, J, K
     }
 
-    public bool HasPlayer
+    /// <summary>
+    /// 自分の部屋から移動可能な部屋を取得
+    /// </summary>
+    /// <param name="myRoom"></param>
+    /// <param name="gameUnit"></param>
+    /// <returns></returns>
+    public List<RoomSymbol> GetMovableRoomsByMyRoom(RoomSymbol myRoom, GameUnit gameUnit)
     {
-        get
+        var result = new List<RoomSymbol>();
+        // プレイヤーかどうかで、右に進めるか判断
+        var isPlayer = gameUnit is Player;
+        // Dをスタート地点、Hをゴールとする
+        switch (myRoom)
         {
-            var result = false;
-            foreach (var gameUnit in CurrentGameUnits)
-            {
-                if (gameUnit is Player)
+            case RoomSymbol.A:
                 {
-                    result = true;
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.B);
+                        result.Add(RoomSymbol.F);
+                    }
+                    result.Add(RoomSymbol.D);
+                    result.Add(RoomSymbol.E);
                     break;
                 }
-            }
-            return result;
+            case RoomSymbol.B:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.C);
+                    }
+                    result.Add(RoomSymbol.A);
+                    result.Add(RoomSymbol.F);
+                    break;
+                }
+            case RoomSymbol.C:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.H);
+                    }
+                    result.Add(RoomSymbol.B);
+                    result.Add(RoomSymbol.F);
+                    break;
+                }
+            case RoomSymbol.D:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.A);
+                        result.Add(RoomSymbol.E);
+                        result.Add(RoomSymbol.I);
+                    }
+                    break;
+                }
+            case RoomSymbol.E:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.F);
+                    }
+                    result.Add(RoomSymbol.A);
+                    result.Add(RoomSymbol.D);
+                    result.Add(RoomSymbol.I);
+                    break;
+                }
+            case RoomSymbol.F:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.C);
+                        result.Add(RoomSymbol.G);
+                        result.Add(RoomSymbol.K);
+                    }
+                    result.Add(RoomSymbol.A);
+                    result.Add(RoomSymbol.B);
+                    result.Add(RoomSymbol.E);
+                    result.Add(RoomSymbol.I);
+                    result.Add(RoomSymbol.J);
+                    break;
+                }
+            case RoomSymbol.G:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.H);
+                    }
+                    result.Add(RoomSymbol.C);
+                    result.Add(RoomSymbol.F);
+                    result.Add(RoomSymbol.K);
+                    break;
+                }
+            case RoomSymbol.H:
+                {
+                    result.Add(RoomSymbol.C);
+                    result.Add(RoomSymbol.G);
+                    result.Add(RoomSymbol.K);
+                    break;
+                }
+            case RoomSymbol.I:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.E);
+                        result.Add(RoomSymbol.J);
+                    }
+                    result.Add(RoomSymbol.D);
+                    result.Add(RoomSymbol.F);
+                    break;
+                }
+            case RoomSymbol.J:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.K);
+                    }
+                    result.Add(RoomSymbol.F);
+                    result.Add(RoomSymbol.I);
+                    break;
+                }
+            case RoomSymbol.K:
+                {
+                    if (isPlayer)
+                    {
+                        result.Add(RoomSymbol.H);
+                    }
+                    result.Add(RoomSymbol.F);
+                    result.Add(RoomSymbol.G);
+                    result.Add(RoomSymbol.J);
+                    break;
+                }
         }
-    }
-
-    [HideInInspector]
-    public List<GameUnit> CurrentGameUnits;
-
-    [SerializeField]
-    public bool IsStart;
-
-    [SerializeField]
-    public bool IsGoal;
-
-    public void DisplayArrowsToRoom()
-    {
-        foreach (var moveTarget in MovableRooms)
-        {
-            var arrow = gameCanvas;
-        }
-    }
-
-    public Room GetEntered(GameUnit gameUnit)
-    {
-        CurrentGameUnits.Add(gameUnit);
-        return this;
-    }
-
-    public void GetLeft(GameUnit gameUnit)
-    {
-        CurrentGameUnits.Remove(gameUnit);
+        return result;
     }
 }
